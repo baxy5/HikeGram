79a2a6e5fc84c0aa4481af08b6e40b23130f7492
baxy5 <bakos.soda@gmail.com>
2022-05-30 21:29:29 +0200

    back end initial

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,8 +1,31 @@
 import Link from "next/link";
+import { useState } from "react";
 import logo from "../styles/Navbar.module.scss";
 import styles from "../styles/Register.module.scss";
+import axios from "axios";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [userData, setUserData] = useState(null);
+
+  const getUserData = async () => {
+    setUserData({ email: email, password: password });
+
+    try {
+      let response = await axios
+        .post("/api/register", userData)
+        .then((res) => console.log(res));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // get sign up data
+  // store data in state
+  // useEffect send data to api
+
   return (
     <div className={styles.register}>
       <div className={logo.navbar}>
@@ -16,11 +39,25 @@ const Register = () => {
       <div className={styles.container}>
         <div className={styles.box}>
           <h1>Sign up</h1>
-          <form action="/api/register" method="post">
+          <form method="post" onSubmit={getUserData}>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              id="email"
+              required
+            />
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              required
+            />
             <div>
               <button type="submit">Submit</button>
             </div>
