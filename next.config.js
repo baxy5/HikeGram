2c7357b4f9494912ada37476ef5e9437bfa906bd
baxy5 <bakos.soda@gmail.com>
2022-05-31 15:48:20 +0200

    mongodb connection

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MONGO_URI:
+      "mongodb+srv://bakszy:bakszy@cluster0.ov4ny.mongodb.net/?retryWrites=true&w=majority",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,3 +1,15 @@
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb+srv://bakszy:futball7@cluster0.ov4ny.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+
+  console.log("Successful connection!");
+  db.close();
+});
+
 export default function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
