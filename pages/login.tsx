e11a022a0a22fc9b4a958d15b1a9a4f7e4679335
baxy5 <bakos.soda@gmail.com>
2022-06-12 15:40:04 +0200

    loading animation init

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Register.module.scss";
 import logo from "../styles/Navbar.module.scss";
 import Link from "next/link";
@@ -7,6 +7,16 @@ export const Login = () => {
   interface User {
     email: String;
     password: String;
+  }
+
+  let [isLoading, setLoading] = useState(false);
+
+  function loadingAnimation() {
+    setLoading(true);
+    setInterval(() => {
+      setLoading(false);
+      console.log("set default");
+    }, 3000);
   }
 
   const userVerification = async (e) => {
@@ -40,23 +50,31 @@ export const Login = () => {
       </div>
 
       <div className={styles.container}>
-        <div className={styles.box}>
-          <h1>Log in</h1>
-          <form method="post" onSubmit={userVerification}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required />
-            <div>
-              <button type="submit">Log in</button>
-            </div>
-          </form>
-          <Link href="/register">
-            <a className={styles.linking}>
-              Sign up if you don't have an account.
-            </a>
-          </Link>
-        </div>
+        {isLoading ? (
+          <div className={styles.loading}>
+            <p>Signing in...</p>
+          </div>
+        ) : (
+          <div className={styles.box}>
+            <h1>Log in</h1>
+            <form method="post" onSubmit={userVerification}>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" required />
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" required />
+              <div>
+                <button type="submit" onClick={loadingAnimation}>
+                  Log in
+                </button>
+              </div>
+            </form>
+            <Link href="/register">
+              <a className={styles.linking}>
+                Sign up if you don't have an account.
+              </a>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
